a69a962603ceb6c724c33c410c7b8236eb71bbba
Chris Allen <christopherallen777@gmail.com>
2017-06-21 13:34:31 -0500

    Wicker branding added. Still need to work on flash cards being correct

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 // Memory Game
-// © 2014 Nate Wiley
-// License -- MIT
+// © 2017 Chris Allen
+// License -- Harris Media Roger Wicker
 // best in full screen, works on phones/tablets (min height for game is 500px..) enjoy ;)
-// Follow me on Codepen
+// Follow me on github [ChristopherAllen777] and linkedin
 
 (function(){
 	
 	var Memory = {
-
+		// Setting up for gameplay
 		init: function(cards){
 			this.$game = $(".game");
 			this.$modal = $(".modal");
@@ -17,7 +17,7 @@
 			this.shuffleCards(this.cardsArray);
 			this.setup();
 		},
-
+		// function for shuffling cards
 		shuffleCards: function(cardsArray){
 			this.$cards = $(this.shuffle(this.cardsArray));
 		},
@@ -28,7 +28,7 @@
 			this.$memoryCards = $(".card");
 			this.binding();
 			this.paused = false;
-     	this.guess = null;
+     		this.guess = null;
 		},
 
 		binding: function(){
@@ -102,6 +102,8 @@
 	    	return array;
 		},
 
+
+		// Correct build function()
 		buildHTML: function(){
 			var frag = '';
 			this.$cards.each(function(k, v){
@@ -114,11 +116,11 @@
 			});
 			return frag;
 		}
-	};
+	};// var Memory = {} end tag
 
 	var cards = [
 		{
-			name: "php",
+			name: "“It is clear that our current fleet of 275 ships is insufficient to address the security challenges we face today and that we anticipate in the future.”",
 			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/php-logo_1.png",
 			id: 1,
 		},
@@ -127,56 +129,56 @@
 			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/css3-logo.png",
 			id: 2
 		},
-		{
-			name: "html5",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
-			id: 3
-		},
-		{
-			name: "jquery",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
-			id: 4
-		}, 
-		{
-			name: "javascript",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
-			id: 5
-		},
-		{
-			name: "node",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
-			id: 6
-		},
-		{
-			name: "photoshop",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
-			id: 7
-		},
-		{
-			name: "python",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
-			id: 8
-		},
-		{
-			name: "rails",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
-			id: 9
-		},
-		{
-			name: "sass",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
-			id: 10
-		},
-		{
-			name: "sublime",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
-			id: 11
-		},
-		{
-			name: "wordpress",
-			img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
-			id: 12
-		},
+		// {
+		// 	name: "html5",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/html5-logo.png",
+		// 	id: 3
+		// },
+		// {
+		// 	name: "jquery",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/jquery-logo.png",
+		// 	id: 4
+		// }, 
+		// {
+		// 	name: "javascript",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/js-logo.png",
+		// 	id: 5
+		// },
+		// {
+		// 	name: "node",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/nodejs-logo.png",
+		// 	id: 6
+		// },
+		// {
+		// 	name: "photoshop",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/photoshop-logo.png",
+		// 	id: 7
+		// },
+		// {
+		// 	name: "python",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/python-logo.png",
+		// 	id: 8
+		// },
+		// {
+		// 	name: "rails",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/rails-logo.png",
+		// 	id: 9
+		// },
+		// {
+		// 	name: "sass",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png",
+		// 	id: 10
+		// },
+		// {
+		// 	name: "sublime",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sublime-logo.png",
+		// 	id: 11
+		// },
+		// {
+		// 	name: "wordpress",
+		// 	img: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/wordpress-logo.png",
+		// 	id: 12
+		// },
 	];
     
 	Memory.init(cards);
